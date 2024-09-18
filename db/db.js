576dd7aaf808f7cec62287ab46961ec7835db8e4
Dipak Zende <114248721+dipak307@users.noreply.github.com>
2024-09-18 18:22:23 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -3,7 +3,12 @@ const mongoose=require("mongoose");
 const db=async () =>{
     try {
          mongoose.set('strictQuery',false)
-         await mongoose.connect(process.env.MONGO_DB_URL)
+         await mongoose.connect(process.env.MONGO_DB_URL,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000,  // Increase timeout to 30 seconds
+  socketTimeoutMS: 45000            // Socket timeout for longer operations
+})
          console.log("Db connected");
     } catch (error) {
         console.log("DB Connection Error");
